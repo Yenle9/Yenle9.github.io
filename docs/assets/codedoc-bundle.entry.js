dc9608c158e78ee078ddfa50f1f4e5a8f52c0590
Yen Le <lengocyen.dvt@gmail.com>
2022-11-13 16:35:31 +0700

    Merge doc

---
+++ docs/assets/codedoc-bundle.entry.js
@@ -0,0 +1,42 @@
+import { getRenderer } from '/Users/mac/Projects/yenle9.github.io/.codedoc/node_modules/@codedoc/core/dist/es6/transport/renderer.js';
+import { initJssCs } from '/Users/mac/Projects/yenle9.github.io/.codedoc/node_modules/@codedoc/core/dist/es6/transport/setup-jss.js';initJssCs();
+import { installTheme } from '/Users/mac/Projects/yenle9.github.io/.codedoc/content/theme.ts';installTheme();
+import { codeSelection } from '/Users/mac/Projects/yenle9.github.io/.codedoc/node_modules/@codedoc/core/dist/es6/components/code/selection.js';codeSelection();
+import { sameLineLengthInCodes } from '/Users/mac/Projects/yenle9.github.io/.codedoc/node_modules/@codedoc/core/dist/es6/components/code/same-line-length.js';sameLineLengthInCodes();
+import { initHintBox } from '/Users/mac/Projects/yenle9.github.io/.codedoc/node_modules/@codedoc/core/dist/es6/components/code/line-hint/index.js';initHintBox();
+import { initCodeLineRef } from '/Users/mac/Projects/yenle9.github.io/.codedoc/node_modules/@codedoc/core/dist/es6/components/code/line-ref/index.js';initCodeLineRef();
+import { initSmartCopy } from '/Users/mac/Projects/yenle9.github.io/.codedoc/node_modules/@codedoc/core/dist/es6/components/code/smart-copy.js';initSmartCopy();
+import { copyHeadings } from '/Users/mac/Projects/yenle9.github.io/.codedoc/node_modules/@codedoc/core/dist/es6/components/heading/copy-headings.js';copyHeadings();
+import { contentNavHighlight } from '/Users/mac/Projects/yenle9.github.io/.codedoc/node_modules/@codedoc/core/dist/es6/components/page/contentnav/highlight.js';contentNavHighlight();
+import { loadDeferredIFrames } from '/Users/mac/Projects/yenle9.github.io/.codedoc/node_modules/@codedoc/core/dist/es6/transport/deferred-iframe.js';loadDeferredIFrames();
+import { smoothLoading } from '/Users/mac/Projects/yenle9.github.io/.codedoc/node_modules/@codedoc/core/dist/es6/transport/smooth-loading.js';smoothLoading();
+import { tocHighlight } from '/Users/mac/Projects/yenle9.github.io/.codedoc/node_modules/@codedoc/core/dist/es6/components/page/toc/toc-highlight.js';tocHighlight();
+import { postNavSearch } from '/Users/mac/Projects/yenle9.github.io/.codedoc/node_modules/@codedoc/core/dist/es6/components/page/toc/search/post-nav/index.js';postNavSearch();
+import { copyLineLinks } from '/Users/mac/Projects/yenle9.github.io/.codedoc/node_modules/@codedoc/core/dist/es6/components/code/line-links/copy-line-link.js';copyLineLinks();
+import { gatherFootnotes } from '/Users/mac/Projects/yenle9.github.io/.codedoc/node_modules/@codedoc/core/dist/es6/components/footnote/gather-footnotes.js';gatherFootnotes();
+import { ToCPrevNext } from '/Users/mac/Projects/yenle9.github.io/.codedoc/node_modules/@codedoc/core/dist/es6/components/page/toc/prevnext/index.js';
+import { ToCToggle } from '/Users/mac/Projects/yenle9.github.io/.codedoc/node_modules/@codedoc/core/dist/es6/components/page/toc/toggle/index.js';
+import { DarkModeSwitch } from '/Users/mac/Projects/yenle9.github.io/.codedoc/node_modules/@codedoc/core/dist/es6/components/darkmode/index.js';
+import { ConfigTransport } from '/Users/mac/Projects/yenle9.github.io/.codedoc/node_modules/@codedoc/core/dist/es6/transport/config.js';
+import { TabSelector } from '/Users/mac/Projects/yenle9.github.io/.codedoc/node_modules/@codedoc/core/dist/es6/components/tabs/selector.js';
+import { CollapseControl } from '/Users/mac/Projects/yenle9.github.io/.codedoc/node_modules/@codedoc/core/dist/es6/components/collapse/collapse-control.js';
+
+const components = {
+  'xPrx5TBfxK+S+Lk3TkQfMg==': ToCPrevNext,
+  'W7/uO/QlIDcceiaQAyrVSQ==': ToCToggle,
+  'fGdZZSfxm9t85jrjF6xaiw==': DarkModeSwitch,
+  'cu6tdSfoUM/BhFZSQBNV8A==': ConfigTransport,
+  'jepyoyrTl3B37100wEwZyw==': TabSelector,
+  'QO9W4ZO+Z4Qg7gRLK5v3Vg==': CollapseControl
+};
+
+const renderer = getRenderer();
+const ogtransport = window.__sdh_transport;
+window.__sdh_transport = function(id, hash, props) {
+  if (hash in components) {
+    const target = document.getElementById(id);
+    renderer.render(renderer.create(components[hash], props)).after(target);
+    target.remove();
+  }
+  else if (ogtransport) ogtransport(id, hash, props);
+}
